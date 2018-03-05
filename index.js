5c991da2caf2b78e8c6bcc4b3cc262d3431c1d5e
Christina HSu <christinahsu.th@gmail.com>
2018-03-05 20:23:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function shout(hello) {
   return hello.toUpperCase()
-};
+}
 
 function whiper(hello) {
   return hello.toLowerCase()
-};
+}
 
 function logShout(hello) {
   console.log(hello.toUpperCase)
-};
+}
